3d5ca4d9396ff6b65cde5760bae52c54b377911a
Md Tabish <mdtabish@Mds-MacBook-Air.local>
2024-08-18 01:57:50 +0530

    changes done

--- src/Components/SnackbarToast.jsx
+++ src/Components/SnackbarToast.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-import Slide from '@mui/material/Slide';
 
 const SnackbarToast = ({ triggerOpen, message, severity, customStyles }) => {
     const [open, setOpen] = useState(false);
@@ -19,16 +18,11 @@ const SnackbarToast = ({ triggerOpen, message, severity, customStyles }) => {
         }
     }, [triggerOpen]);
 
-    const SlideTransition = (props) => {
-        return <Slide {...props} direction="left" />; 
-    };
-
     return (
         <Snackbar 
         open={open} 
         autoHideDuration={5000} 
         onClose={handleClose}  
-        TransitionComponent={SlideTransition} 
         anchorOrigin={{ vertical: 'top', horizontal: 'right' }} 
         sx={{ zIndex: 9999 }}>
             <Alert
