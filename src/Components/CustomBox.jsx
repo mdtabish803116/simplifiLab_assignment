1f90b2baef4abdc2b5550c20598406bf1ce2ad34
Md Tabish <mdtabish@Mds-MacBook-Air.local>
2024-08-21 18:18:04 +0530

    changes done

--- src/Components/CustomBox.jsx
+++ src/Components/CustomBox.jsx
@@ -3,10 +3,7 @@ import { Link } from 'react-router-dom'
 
 function CustomBox({title , email , buttonText ,text}) {
   return (
-    <div style = {{position:"absolute" , textAlign: "center", color: "black", 
-     top: "70px",width:"30vw", border: "1px solid rgb(236, 147, 36)", 
-     boxShadow:"rgba(0, 0, 0, 0.3) 0px 4px 8px",
-     borderRadius:"12px",fontSize:"0.9rem", padding:"10px", background:"white"}} 
+    <div className = "customBoxContainer"
      onClick = {(e) => e.stopPropagation()}>
           <p>{title}</p>
           <p style = {{color: "rgba(0 , 0 , 0, 0.6)"}} >{email}</p>
